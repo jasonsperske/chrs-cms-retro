1cabd15ae9245820973b950a30bcb69668b94b40
Jason Sperske <jason@sperske.com>
2024-11-23 11:00:43 -0800

    moved drop handler to form element (easier target)

--- static/cms.mjs
+++ static/cms.mjs
@@ -95,11 +95,11 @@ function setupUploader(el) {
         submitButton.disabled = filecache.length === 0
     }
 
-    imageBucket.addEventListener('dragover', function (event) {
+    el.addEventListener('dragover', function (event) {
         event.preventDefault()
     }, false)
 
-    imageBucket.addEventListener('drop', function (/** @type{DragEvent} */ event) {
+    el.addEventListener('drop', function (/** @type{DragEvent} */ event) {
         event.preventDefault()
         if (event.dataTransfer.items) {
             updateThumbnails([
